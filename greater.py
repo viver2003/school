640e16b0f96eeb0221c5466b67d321378c3d348d
viver2003 <novi@noviorion.com>
2015-11-18 19:34:31 -0800

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+x = int(input('one number'))
+y = int(input('second number'))
+if (x > y):
+    print "%s is greater than %s" % (x,y)
+if (x < y):
+    print "%s is lesser than %s" % (x,y)
+if (x == y):
+    print "%s and %s are of equil value" % (x,y)
+
