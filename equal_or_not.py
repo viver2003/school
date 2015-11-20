3d89f68f40524fcb47d37680e630d9fe43bfacea
viver2003 <novi@noviorion.com>
2015-11-19 16:36:46 -0800

    Create equal_or_not.py

---
+++ equal_or_not.py
@@ -0,0 +1,8 @@
+a = int(input('first number'))
+b = int(input('second number'))
+c = int(input('third number'))
+d = int(input('forth number'))
+if (a == b) and (a == c) and (a == d):
+    print 'These four numbers are equil!!!'
+else:
+    print 'These four numbers are not equil.'
